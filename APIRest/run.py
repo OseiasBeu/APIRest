b0654b41cce7f1184f20cc4cc1be925ab335e1d5
Oseias Beu <oseiasbeu@outlook.com>
2018-12-23 00:59:25 -0200

    Criar base de dados e primeira tabela

---
+++ APIRest/run.py
@@ -0,0 +1,11 @@
+from app import app
+
+#Importando Rotas
+from app.routes import *
+
+#Importando Models
+from app.models import usuario
+
+if __name__ == '__main__':
+    app.run(port=8080, debug=True)
+
